68a93d57b607848d2e0751db66b043a868c7417b
chenlili <chelili@pset.suntec.net>
2016-12-07 20:49:46 +0800

    [1] finish Recursion
    Change-Id: Icd7b0b51103c3d4d6794b174c67902206426580c

--- recfun/src/main/scala/recfun/Main.scala
+++ recfun/src/main/scala/recfun/Main.scala
@@ -13,15 +13,52 @@ object Main {
   /**
    * Exercise 1
    */
-    def pascal(c: Int, r: Int): Int = ???
+    def pascal(c: Int, r: Int): Int = 
+      if (r <= 0 || c <= 0 || c >= r) {
+        1
+      } else {
+        pascal(c, r - 1) + pascal(c - 1, r - 1)
+      }
   
   /**
    * Exercise 2
    */
-    def balance(chars: List[Char]): Boolean = ???
+    def balance(chars: List[Char]): Boolean = doBalance(0, chars)
+
+    def doBalance(leftParentNum: Int, chars: List[Char]): Boolean = {
+        if (chars.isEmpty && leftParentNum == 0) {
+            true
+        } else if (chars.isEmpty && leftParentNum != 0) {
+            false
+        } else {
+            if (chars.head == '(') {
+                doBalance(leftParentNum + 1, chars.tail)
+            } else if (chars.head == ')') {
+                if (leftParentNum - 1 < 0) {
+                    false
+                } else {
+                    doBalance(leftParentNum - 1, chars.tail)
+                }
+            } else {
+                doBalance(leftParentNum, chars.tail)
+            }
+        }
+    }
   
   /**
    * Exercise 3
    */
-    def countChange(money: Int, coins: List[Int]): Int = ???
+    def countChange(money: Int, coins: List[Int]): Int =
+        doCountChange(money, coins.sorted)
+    def doCountChange(money: Int, coins: List[Int]): Int = 
+        if (money > 0 && coins.isEmpty) {
+            0
+        } else if (money < coins.head) {
+            doCountChange(money, coins.tail)
+        } else if (money == coins.head) {
+            1
+        } else {
+            doCountChange(money - coins.head, coins) +
+                doCountChange(money, coins.tail)
+        }
   }
